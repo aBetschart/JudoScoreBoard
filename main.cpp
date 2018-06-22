90473e256c56584235ad229cef864de4430daa46
Aaron Betschart <aaron.betschart@swissonline.ch>
2018-06-23 00:14:38 +0200

    main: Main starts only the main state machine now.

--- main.cpp
+++ main.cpp
@@ -3,32 +3,11 @@
  */
 
 
-#include "Obj/JudoDisplay/JudoDisplay.h"
-#include "Obj/OsaekTime/OsaekTime.h"
-#include "Obj/mainTime/mainTime.h"
-#include "Obj/Judo.h"
+#include "Obj/FightCtrl/FightCtrl.h"
 
 int main(void)
 {
-    FighterScore score[2];
-
-    score[0].ippon = true;
-    score[0].wazari = 5;
-    score[0].shido = 2;
-
-    score[1].ippon = false;
-    score[1].wazari = 1;
-    score[1].shido = 1;
-
-    Obj::JudoDisplay displ;
-    Obj::OsaekTime osaekTime(&displ);
-    Obj::MainTime mainTime(&displ);
-
-    displ.setScoreDigits(blue, score[0]);
-    displ.setScoreDigits(white, score[1]);
-
-    osaekTime.start();
-    mainTime.start();
+    Obj::FightCtrl fightCtrl;
 
     while(1);
 }
