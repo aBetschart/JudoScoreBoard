19e950ff78ef971967a490ee85cfaa76350cde1d
Aaron Betschart <aaron.betschart@swissonline.ch>
2018-08-30 11:46:28 +0200

    Renamed the Registers in each Test.

--- test/Register/SoftwareRegisterTest.cpp
+++ test/Register/SoftwareRegisterTest.cpp
@@ -30,7 +30,7 @@ TEST_F( SoftwareRegisterTest, namingTet )
 TEST_F( SoftwareRegisterTest, defaultValueTest1 )
 {
 	uint16_t defaultValue = 0x001A;
-	Register::SoftwareRegister<uint16_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint16_t> testedRegister( "DefaultValueTest1", defaultValue );
 
 	uint16_t expectedValue = defaultValue;
 	uint16_t actualValue   = testedRegister.getVal();
@@ -41,7 +41,7 @@ TEST_F( SoftwareRegisterTest, defaultValueTest1 )
 TEST_F( SoftwareRegisterTest, defaultValueTest2 )
 {
 	uint16_t defaultValue = 0;
-	Register::SoftwareRegister<uint16_t> testedRegister( "TestRegister" );
+	Register::SoftwareRegister<uint16_t> testedRegister( "DefaultValueTest2" );
 
 	uint16_t expectedValue = defaultValue;
 	uint16_t actualValue   = testedRegister.getVal();
@@ -52,7 +52,7 @@ TEST_F( SoftwareRegisterTest, defaultValueTest2 )
 TEST_F( SoftwareRegisterTest, clearBitsTest1 )
 {
 	uint16_t defaultValue = 0xA321;
-	Register::SoftwareRegister<uint16_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint16_t> testedRegister( "ClearBitsTest1", defaultValue );
 
 	uint16_t clearedBits = 0x0000;
 	testedRegister.clearBits( clearedBits );
@@ -66,7 +66,7 @@ TEST_F( SoftwareRegisterTest, clearBitsTest1 )
 TEST_F( SoftwareRegisterTest, clearBitsTest2 )
 {
 	uint16_t defaultValue = 0x242B;
-	Register::SoftwareRegister<uint16_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint16_t> testedRegister( "ClearBitsTest2", defaultValue );
 
 	uint16_t clearedBits = 0xFFFF;
 	testedRegister.clearBits( clearedBits );
@@ -80,7 +80,7 @@ TEST_F( SoftwareRegisterTest, clearBitsTest2 )
 TEST_F( SoftwareRegisterTest, clearBitsTest3 )
 {
 	uint16_t defaultValue = 0x242B;
-	Register::SoftwareRegister<uint16_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint16_t> testedRegister( "ClearBitsTest3", defaultValue );
 
 	uint16_t clearedBits = 0xF0A0;
 	testedRegister.clearBits( clearedBits );
@@ -94,7 +94,7 @@ TEST_F( SoftwareRegisterTest, clearBitsTest3 )
 TEST_F( SoftwareRegisterTest, setBitsTest1 )
 {
 	uint16_t defaultValue = 0x242B;
-	Register::SoftwareRegister<uint16_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint16_t> testedRegister( "SetBitsTest1", defaultValue );
 
 	uint16_t setBits = 0x0000;
 	testedRegister.setBits( setBits );
@@ -108,7 +108,7 @@ TEST_F( SoftwareRegisterTest, setBitsTest1 )
 TEST_F( SoftwareRegisterTest, setBitsTest2 )
 {
 	uint16_t defaultValue = 0xAC13;
-	Register::SoftwareRegister<uint16_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint16_t> testedRegister( "SetBitsTest2", defaultValue );
 
 	uint16_t setBits = 0xFFFF;
 	testedRegister.setBits( setBits );
@@ -122,7 +122,7 @@ TEST_F( SoftwareRegisterTest, setBitsTest2 )
 TEST_F( SoftwareRegisterTest, setBitsTest3 )
 {
 	uint16_t defaultValue = 0x56ED;
-	Register::SoftwareRegister<uint16_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint16_t> testedRegister( "SetBitsTest3", defaultValue );
 
 	uint16_t setBits = 0xF023;
 	testedRegister.setBits( setBits );
@@ -136,7 +136,7 @@ TEST_F( SoftwareRegisterTest, setBitsTest3 )
 TEST_F( SoftwareRegisterTest, insertTest1 )
 {
 	uint32_t defaultValue = 0x5060E0D0;
-	Register::SoftwareRegister<uint32_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint32_t> testedRegister( "InsertTest1", defaultValue );
 
 	uint32_t insertedValue = 0x00000000;
 	testedRegister.insert( insertedValue );
@@ -150,7 +150,7 @@ TEST_F( SoftwareRegisterTest, insertTest1 )
 TEST_F( SoftwareRegisterTest, insertTest2 )
 {
 	uint32_t defaultValue = 0x5060E0D0;
-	Register::SoftwareRegister<uint32_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint32_t> testedRegister( "InsertTest2", defaultValue );
 
 	uint32_t insertedValue = 0xFFFFFFFF;
 	testedRegister.insert( insertedValue );
@@ -164,7 +164,7 @@ TEST_F( SoftwareRegisterTest, insertTest2 )
 TEST_F( SoftwareRegisterTest, insertTest3 )
 {
 	uint32_t defaultValue = 0x5060E0D0;
-	Register::SoftwareRegister<uint32_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint32_t> testedRegister( "InsertTest3", defaultValue );
 
 	uint32_t insertedValue = 0x23ACFF09;
 	testedRegister.insert( insertedValue );
@@ -178,7 +178,7 @@ TEST_F( SoftwareRegisterTest, insertTest3 )
 TEST_F( SoftwareRegisterTest, checkBitsTest1 )
 {
 	uint32_t defaultValue = 0x5060E0D0;
-	Register::SoftwareRegister<uint32_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint32_t> testedRegister( "CheckBitsTest1", defaultValue );
 
 	uint32_t checkedBits = 0x00000000;
 
@@ -189,7 +189,7 @@ TEST_F( SoftwareRegisterTest, checkBitsTest1 )
 TEST_F( SoftwareRegisterTest, checkBitsTest2 )
 {
 	uint32_t defaultValue = 0x5060E0D0;
-	Register::SoftwareRegister<uint32_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint32_t> testedRegister( "CheckBitsTest2", defaultValue );
 
 	uint32_t checkedBits = ~defaultValue;
 
@@ -200,7 +200,7 @@ TEST_F( SoftwareRegisterTest, checkBitsTest2 )
 TEST_F( SoftwareRegisterTest, checkBitsTest3 )
 {
 	uint32_t defaultValue = 0x5060E0D0;
-	Register::SoftwareRegister<uint32_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint32_t> testedRegister( "CheckBitsTest3", defaultValue );
 
 	uint32_t checkedBits = defaultValue;
 
@@ -211,7 +211,7 @@ TEST_F( SoftwareRegisterTest, checkBitsTest3 )
 TEST_F( SoftwareRegisterTest, checkBitsTest4 )
 {
 	uint32_t defaultValue = 0x5060E0D0;
-	Register::SoftwareRegister<uint32_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint32_t> testedRegister( "CheckBitsTest4", defaultValue );
 
 	uint32_t checkedBits = 0x00000001;
 
@@ -222,7 +222,7 @@ TEST_F( SoftwareRegisterTest, checkBitsTest4 )
 TEST_F( SoftwareRegisterTest, checkBitsTest5 )
 {
 	uint32_t defaultValue = 0x5060E0D3;
-	Register::SoftwareRegister<uint32_t> testedRegister( "TestRegister", defaultValue );
+	Register::SoftwareRegister<uint32_t> testedRegister( "CheckBitsTest5", defaultValue );
 
 	uint32_t checkedBits = 0x00000001;
 
